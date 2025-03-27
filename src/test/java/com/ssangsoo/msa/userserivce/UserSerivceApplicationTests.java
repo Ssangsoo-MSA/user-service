9d5fb4db3e2a65e3124d6575d4fe171534d2eb15
SsangSoo <kss10080@naver.com>
2025-03-28 00:09:01 +0900

    user-service project init

---
+++ src/test/java/com/ssangsoo/msa/userserivce/UserSerivceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssangsoo.msa.userserivce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSerivceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
